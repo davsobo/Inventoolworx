ec97d7e9b283986b0c21c111fd389595f332d9ab
davsobo <aureliousvayne57@gmail.com>
2018-03-15 16:24:00 +0700

    AWYISS AWNOUH TAKEN and then PRESS TO CONTINUE /**LyfeGood**/

--- app/src/main/java/android/studio/inventoolworx/TakenActivity.java
+++ app/src/main/java/android/studio/inventoolworx/TakenActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,38 +35,61 @@ public class TakenActivity extends AppCompatActivity {
 
     private TableRow CreateRow (Map<String,String> data)
     {
+        final float scale = getApplicationContext().getResources().getDisplayMetrics().density;
+        int trHeight = (int) (80 * scale + 0.5f);
+        int trWidth = (int) (67 * scale + 0.5f);
         TableRow result = new TableRow(this);
-        result.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
+//        TableRow.LayoutParams tlparams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,trHeight);
+        TableRow.LayoutParams tlparams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, trHeight);
+        result.setLayoutParams(tlparams);
+//      result.setMinimumHeight(100);
+//      result.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
 
+        /**Label*/
         TextView text = new TextView(this);
-        text.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 2f));
-        //text.setBackgroundResource(R.drawable.border);
-        text.setGravity(Gravity.RIGHT | Gravity.CENTER);
+        text.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 6f));
+        text.setGravity(Gravity.CENTER | Gravity.TOP);
+        text.setMinHeight(300);
         text.setSingleLine(false);
-        text.setPadding(10,10,10,10);
-        text.setText("Tipe:\n"+ "Ukuran:\n"+ "Bahan:\n"+ "Merk:\n"+ "Jumlah:\n"+ "Lokasi:");
+        text.setText("Tipe:"+ data.get("tipe")+
+                "\nUkuran:"+ data.get("ukuran")+
+                "\nBahan:"+ data.get("bahan")+
+                "\nMerk:"+  data.get("merk")+
+                "\nJumlah:"+ data.get("jumlah")+
+                "\nLokasi:" + data.get("lokasi"));
+//        text.setText("Tipe:\n"+ "Ukuran:\n"+ "Bahan:\n"+ "Merk:\n"+ "Jumlah:\n"+ "Lokasi:");
         result.addView(text);
-        TextView fill = new TextView(this);
-        fill.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 4f));
-//        fill.setBackgroundResource(R.drawable.border);
-        fill.setGravity(Gravity.LEFT | Gravity.CENTER);
-        fill.setSingleLine(false);
-        fill.setPadding(10,10,10,10);
-        fill.setText(data.get("tipe")+"\n"+ data.get("ukuran")+"\n"+ data.get("bahan")+"\n"+ data.get("merk")+"\n"+ data.get("jumlah")+"\n"+ data.get("lokasi"));
-        result.addView(fill);
 
+//        /**Item*/
+//        TextView fill = new TextView(this);
+//        fill.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 2f));
+//        fill.setGravity(Gravity.LEFT | Gravity.CENTER| Gravity.TOP);
+//        fill.setSingleLine(false);
+//        text.setMinHeight(150);
+//        fill.setText(data.get("tipe")+"\n"+ data.get("ukuran")+"\n"+ data.get("bahan")+"\n"+ data.get("merk")+"\n"+ data.get("jumlah")+"\n"+ data.get("lokasi"));
+//        result.addView(fill);
+
+        /**Ambil*/
         EditText taken = new EditText(this);
-        taken.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1f));
-        taken.setGravity(Gravity.CENTER | Gravity.CENTER);
+        taken.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, trHeight, 1f));
+//        taken.setBackgroundResource(R.drawable.border);
+        taken.setGravity(Gravity.CENTER);
         taken.setHint("0");
         result.addView(taken);
         Button cancel = new Button(this);
 
-        cancel.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-        cancel.setGravity(Gravity.CENTER | Gravity.CENTER);
-        cancel.setText("X");
+        /**Batal*/
+        cancel.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, trHeight, 1f));
+        cancel.setGravity(Gravity.CENTER );
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+        cancel.setText("X");
         result.addView(cancel);
+
         return result;
     }
 }
